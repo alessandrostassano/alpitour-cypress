555a03af7ad99acac6267b34d62802ed88c48aae
Stassano Alessandro <Alessandro.Stassano@alpitourworld.it>
2022-10-11 14:49:35 +0200

    test finished. Add exception at file index.js support

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,3 +1,7 @@
 import './commands'
 import '@shelex/cypress-allure-plugin';
 require('cypress-xpath')
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from failing the test
+    return false
+  })
